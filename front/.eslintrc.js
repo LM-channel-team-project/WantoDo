af9332428be338b59523a960b93cd7b7bdb3b2b7
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-13 14:53:37 +0900

    [Chore] front 프리티어 세팅
    - .prettierrc.js 세팅
    - .eslintrc.js 수정
    - eslint 디펜던시 dev로 변경
    - eslint 타입스크립트 플러그인 제거

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    'no-console': 0,
+  },
 };
