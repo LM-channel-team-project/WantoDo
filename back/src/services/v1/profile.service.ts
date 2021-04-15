e1251fc71c9c9a687ae0e2ac6887dbdc3b69f7a8
hyunbin1 <kimgusqls1@gmail.com>
2021-04-16 00:55:09 +0900

    [add] 프로필 조회 서비스.ts 코드 추가

--- back/src/services/v1/profile.service.ts
+++ back/src/services/v1/profile.service.ts
@@ -1,10 +1,10 @@
 import { v4 as uuidV4 } from 'uuid';
 
 import * as Types from "../../common/types";
-import { users } from '../../models/user.model';
+import { users, theme } from '../../models/user.model';
 
 
-// 21//4/13 by 현빈
+// 21/4/13, 21/4/16 by 현빈
 // 로그인 유저인지는 models/goolge에서 확인함
 // => 로그인 유저 플랫폼, 닉네임, 토큰? 정기결제 확인해줌.
 export const getProfile = async (user: {
@@ -14,18 +14,44 @@ export const getProfile = async (user: {
     platform: Types.platform,
     name: string,
 }) => {
-
     try {
-        const user = await users.findOne({
-            platform: users.platform,
-            platformId: user.platformId,
-            closeAccountFlag: false
+        // 21/4/16 유저에게 보내줄 데이터들 함수 정의 by 현빈
+        let returnToUser = {
+            email: '',
+            platform: '',
+            nickname: '',
+            motto: '',
+            profileImageUrl: '',
+        };
 
-        })
+
+
+        // 21/4/16 Db에서 유저 찾기 + 정보 가져오기 by 현빈
+        const findDatabaseUser = await users.findOne({
+            platform: user.platform,
+            platformId: user.platformId,
+            closeAccountFlag: false,
+        }).exec();
+
+        if (!findDatabaseUser) throw new Error('');
+
+        returnToUser = {
+            email: findDatabaseUser.email,
+            platform: findDatabaseUser.platform,
+            motto: findDatabaseUser.motto,
+            profileImageUrl: findDatabaseUser.profileImageUrl,
+            nickname: findDatabaseUser.nickname || findDatabaseUser.name,
+        }
+
+        return {
+            message: '유저정보가 표시되었습니다.',
+            user: returnToUser,
+        }
+    } catch (err) {
+        throw err;
     }
 
-
-}
+};
 
 
 
