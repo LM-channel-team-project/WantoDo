3f9fbb14ea8a90a6fceb1535f77b63147e756167
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-15 14:02:50 +0900

    [Add] IconButton 컴포넌트 추가
    - 버튼 컴포넌트를 재사용하여 IconButton 컴포넌트 추가
    - IconButton.module.css 추가
    - 스타일링 및 테스트 위해 App.jsx에 IconButton 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import { FiCalendar } from 'react-icons/fi';
 import Button from './components/Button';
+import IconButton from './components/IconButton';
 import InputBox from './components/InputBox';
 
 function App() {
   return (
     <div className="app">
       <InputBox labelText="시작" name="start" type="text" />
-      <Button type="submit" onClick={event => console.log(event.target.type)}>
+      <Button
+        type="submit"
+        styleName="calendar"
+        onClick={event => console.log(event.target.type)}
+      >
         일반 버튼1
       </Button>
       <Button onClick={event => console.log(event.target.type)}>
         일반 버튼2
       </Button>
+      <IconButton
+        Icon={FiCalendar}
+        type="submit"
+        onClick={event => console.log(event.target.type)}
+      />
+      <IconButton
+        Icon={FiCalendar}
+        type="submit"
+        styleName="calendar"
+        onClick={event => console.log(event.target.type)}
+      >
+        텍스트 있는 아이콘버튼
+      </IconButton>
     </div>
   );
 }
