e578d53026251d6b76ee6a7e1be0b070814494dd
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-16 16:45:06 +0900

    [Docs] 컴포넌트 주석 수정
    주석 표기법 및 내용 일부 수정

--- front/src/components/Button.jsx
+++ front/src/components/Button.jsx
@@ -3,10 +3,10 @@ import styles from '../styles/Button.module.css';
 
 /**
  * 일반적인 버튼 컴포넌트
- * @param content extends React.Component | string, children으로 전달된, 버튼에 들어갈 내용 컴포넌트 혹은 문자열
- * @param type string, 버튼 타입을 지정하는 문자열 ('button' 또는 'submit')
- * @param styleName string, 기본 버튼 스타일을 변형할 클래스 이름 문자열
- * @param onClick function, 버튼에서 클릭 이벤트가 발생하면 호출될 함수
+ * @param {ReactElement | string} props.children으로 - 버튼에 들어갈 내용 컴포넌트 혹은 문자열
+ * @param {string} props.type - string, 버튼 타입을 지정하는 문자열 ('button' 또는 'submit')
+ * @param {string} props.styleName - string, 기본 버튼 스타일을 변형할 클래스 이름 문자열
+ * @param {Function} props.onClick - function, 버튼에서 클릭 이벤트가 발생하면 호출될 함수
  */
 const Button = ({ children: content, type, styleName, onClick }) => {
   const onButtonClick = event => {
--- front/src/components/ProfileButon.jsx
+++ front/src/components/ProfileButon.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import Button from './Button';
 import ProfileImage from './ProfileImage';
 
+/**
+ * 프로필 이미지 버튼
+ * @param {string} imageURL - 사용자의 프로필 이미지 URL
+ * @param {string} styleName - 공통 스타일링에 대한 변형에 사용할 클래스명 문자열, Button과 ProfileImage에서 적용
+ * @param {Function} onClick - 클릭 이벤트를 핸들링할 함수
+ */
 const ProfileButon = ({ imageURL, styleName, onClick }) => (
   <Button styleName={styleName} onClick={onClick}>
     <ProfileImage imageURL={imageURL} styleName={styleName} />
