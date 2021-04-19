ea8f62a6de7f13c22d8c995b73b1edfd6cc5db48
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-19 21:09:23 +0900

    [Docs] 주석 수정

--- front/src/components/ProfileImage.jsx
+++ front/src/components/ProfileImage.jsx
@@ -3,8 +3,8 @@ import styles from '../styles/ProfileImage.module.css';
 
 /**
  * 사용자 프로필 이미지 컴포넌트
- * @param {string} imageURL - 사용자의 프로필 이미지 URL
- * @param {string} styleName - 공통 스타일링에 대한 변형에 사용할 클래스명 문자열
+ * @param {string} props.imageURL - 사용자의 프로필 이미지 URL
+ * @param {string} props.styleName - 공통 스타일링에 대한 변형에 사용할 클래스명 문자열
  */
 const ProfileImage = ({ imageURL, styleName }) => (
   <img
