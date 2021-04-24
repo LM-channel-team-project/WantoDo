60b9141e8a909b6e427b07077cfbb36525dda0f5
leeseungjin <dltmdwls154@gmail.com>
2021-04-24 11:21:51 +0900

    [Chore]퀵버튼 주석 수정

--- front/src/components/QuickButton.jsx
+++ front/src/components/QuickButton.jsx
@@ -3,9 +3,7 @@ import {FaPlusCircle} from "react-icons/fa";
 import IconButton from "./IconButton";
 
 /** Create by 이승진(dltmdwls154@gmail.com)
- * 항상 우측 하단에 표시되고 클릭시 input 보이게
- * @param {변수타입} props.styleName - 커스텀 스타일을 위한 스타일 네임
- *
+ * 항상 우측 하단에 표시되고 클릭시 input 보이는 버튼
  */
 
 const QuickButton = () => {
