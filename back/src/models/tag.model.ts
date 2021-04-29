d1574978ae54f669a1352e5d85ae576f5d6c5605
hyunbin1 <kimgusqls1@gmail.com>
2021-04-29 13:09:31 +0900

    [FIX] 태그 속성 추가

--- back/src/models/tag.model.ts
+++ back/src/models/tag.model.ts
@@ -11,7 +11,7 @@ const tagSchema = new Schema({
 
     // 태그 식별용 아이디
     tagId: {
-        type: Number,
+        type: String,
         required: true,
     },
 
@@ -20,6 +20,42 @@ const tagSchema = new Schema({
         type: String,
         required: true
     },
+    // 생성 시간
+    createdTimestamp: {
+        type: Number,
+        default: () => Math.floor(+new Date() / 1000),
+    },
+
+    // 업데이트 시간
+    updatedTimestamp: {
+        type: Number,
+        default: () => Math.floor(+new Date() / 1000),
+    },
+
+    // 대표태그 여부
+    representativeTag: {
+        type: Boolean,
+        reuired: true,
+    },
+
+    // 컬러
+    color: {
+        type: String,
+    },
+
+
+    // 삭제 여부
+    whetherDeleteTag: {
+        type: Boolean,
+        default: false,
+    },
+
+    // 삭제 메세지
+    deleteTagMessage: {
+        type: String,
+        default: '',
+    }
+
 
 })
 // interface 관련 정보 참조: https://jaeyeophan.github.io/2017/12/28/TS-4-Interface-in-TypeScript/
@@ -28,6 +64,12 @@ export interface UserTag extends Document {
     userId: string;
     tagId: string;
     tagContents: string;
+    createdTimestamp: number;
+    updatedTimestamp: number;
+    representativeTag: boolean;
+    whetherDeleteTag: boolean;
+    color: string;
+    deleteTagMessage: string;
 }
 
 export const userTags: Model<UserTag> = model('userTag', tagSchema);
