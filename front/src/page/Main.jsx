048caeea8ba37d13e5b39a9ca65a6c5bf1d3e396
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-29 18:46:34 +0900

    [Add] Main 좌측 레이아웃 설정

--- front/src/page/Main.jsx
+++ front/src/page/Main.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Calendar from '../components/Calendar';
+import Input from '../components/Input';
 import TaskList from '../components/TaskList';
 import Layout from '../container/Layout';
 import Navbar from '../container/Navbar';
+import styles from '../styles/page/Main.module.css';
+
+const Left = ({ tasks }) => {
+  return (
+    <div className={styles.left}>
+      <header className={styles.left__header}>
+        <h2>Todo</h2>
+      </header>
+      <div className={styles.left__content}>
+        <TaskList tasks={tasks} />
+      </div>
+      <footer className={styles.left__footer}>
+        <Input />
+      </footer>
+    </div>
+  );
+};
 
 const Main = ({ profile, tasks }) => (
   <Layout
     Side={() => <Navbar profileURL={profile.imageURL} />}
-    Left={() => <TaskList tasks={tasks} />}
+    Left={() => <Left tasks={tasks} />}
     Right={() => <Calendar />}
   />
 );
