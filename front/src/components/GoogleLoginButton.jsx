80f8a73c99ae905364a08708c7bc8de19cada616
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-29 21:06:55 +0900

    [Add] 회원가입 페이지 추가
    - 회원가입 페이지 추가
    - 회원가입 페이지 기본 스타일링
    - textarea 태그 css reset 설정

--- front/src/components/GoogleLoginButton.jsx
+++ front/src/components/GoogleLoginButton.jsx
@@ -3,9 +3,11 @@ import { GoogleLogin } from 'react-google-login';
 import { AiOutlineGoogle } from 'react-icons/ai';
 import IconButton from './IconButton';
 
-const GoogleLoginButton = () => {
+const GoogleLoginButton = ({ onLogin }) => {
   const responseGoogle = (response) => {
+    // 서버에 사용자 정보 전송, 가입 상태 응답 받음
     console.log(response);
+    onLogin(false);
   };
 
   return (
@@ -13,6 +15,7 @@ const GoogleLoginButton = () => {
       clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
       onSuccess={responseGoogle}
       onFailure={responseGoogle}
+      // isSignedIn
       cookiePolicy="single_host_origin"
       render={(props) => (
         <IconButton Icon={AiOutlineGoogle} onClick={props.onClick}>
--- front/src/components/InputBox.jsx
+++ front/src/components/InputBox.jsx
@@ -7,12 +7,23 @@ import styles from '../styles/InputBox.module.css';
  * @param {string} props.labelText - 인풋의 라벨로 표시할 문자열
  * @param {stirng} props.inputType - input의 type 속성 값
  * @param {string} props.inputName - form에서 모든 input의 값을 객체로 받을 때 key로 쓰일 문자열
+ * @param {string} props.afterText - input 뒤에 부가 설명을 표시하는 문자열
  * @param {string} props.placeholder - input의 placeholder 속성 값
  * @param {Function} props.validator - input의 유효성 검사를 수행할 콜백 함수, 반환하는 boolean 값에 따라 상태 변화 여부 결정
  */
-const InputBox = ({ labelText, inputType, inputName, validator, placeholder }) => {
+const InputBox = ({
+  labelText,
+  inputType,
+  inputName,
+  afterText,
+  validator,
+  placeholder,
+  styleName,
+  rows,
+  cols,
+}) => {
   return (
-    <label className={styles.inputBox} htmlFor={styles.label}>
+    <label className={`${styles.inputBox} ${styles.styleName}`} htmlFor={styles.label}>
       <span className={styles.name}>{labelText || ''}</span>
       <Input
         id={styles.label}
@@ -20,7 +31,11 @@ const InputBox = ({ labelText, inputType, inputName, validator, placeholder }) =
         type={inputType}
         placeholder={placeholder}
         validator={validator}
+        styleName={styleName}
+        rows={rows}
+        cols={cols}
       />
+      <span className={styles.text}>{afterText}</span>
     </label>
   );
 };
