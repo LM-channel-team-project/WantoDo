c067a5a6dc2d637a09cb6484972808de476e198c
YoungRong.Oh <youngrong.oh@gmail.com>
2021-04-30 17:50:25 +0900

    [Add] TaskList 표시 형식 선택자 추가

--- front/src/components/TaskList.jsx
+++ front/src/components/TaskList.jsx
@@ -5,10 +5,10 @@ import styles from '../styles/TaskList.module.css';
  * 태스크 리스트
  * @param {Object} props.tasks - 사용자의 태스크 정보를 담고 있는 리스트 객체
  */
-const TaskList = ({ tasks = {} }) => (
+const TaskList = ({ tasks = {}, type }) => (
   <ol className={styles.list}>
     {Object.keys(tasks).map((key) => (
-      <TaskItem key={key} task={tasks[key]} type="daily" />
+      <TaskItem key={key} task={tasks[key]} type={type} />
     ))}
   </ol>
 );
