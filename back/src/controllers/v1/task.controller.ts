3a2beb6ae3dbcb787856ec2328a0d4217ada7ebb
castleMo <eodah4115@gmail.com>
2021-05-02 22:33:21 +0900

    [Add] task 수정 API
    - task 수정 & 삭제 Docs 추가
    - task 수정 API 생성

--- back/src/controllers/v1/task.controller.ts
+++ back/src/controllers/v1/task.controller.ts
@@ -61,7 +61,7 @@ export const createTask = async (req: Request, res: Response, next: NextFunction
 				.withMessage('not properties')
 				.run(req),
 			body('important')
-				.optional()
+				.optional({ nullable: true })
 				.notEmpty()
 				.withMessage('is empty')
 				.bail()
@@ -112,15 +112,94 @@ export const getTasks = (req: Request, res: Response, next: NextFunction) => {
 
 /**
  * @author 강성모(castleMo)
- * @since
+ * @since 21/05/01
  *
  * @param req
  * @param res
  * @param next
  */
-export const updateTask = (req: Request, res: Response, next: NextFunction) => {
+export const updateTask = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		res.status(200).send('update Task');
+		await Promise.all([
+			header('Authorization')
+				.trim()
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isJWT()
+				.withMessage('is not JWT value')
+				.run(req),
+			body('contents')
+				.optional({ checkFalsy: true })
+				.trim()
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isLength({ min: 1, max: 50 })
+				.withMessage('contents length must be greater than 1 or less than 50')
+				.run(req),
+			body('tags')
+				.optional({ checkFalsy: true })
+				.isArray()
+				.withMessage('is not Array value')
+				.bail()
+				.isArray({ min: 1 })
+				.withMessage('Array length must be greater than 1')
+				.run(req),
+			body('tags.*')
+				.trim()
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isString()
+				.withMessage('is not String value')
+				.bail()
+				.isUUID('4')
+				.withMessage('is not UUID version4 value')
+				.run(req),
+			body('period')
+				.optional({ checkFalsy: true })
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isObject()
+				.withMessage('is not Object value')
+				.custom((value) => {
+					return value.start !== undefined && value.end !== undefined;
+				})
+				.withMessage('not properties')
+				.run(req),
+			body('important')
+				.optional({ nullable: true })
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isInt({ min: 0, max: 3 })
+				.withMessage('is not Number value')
+				.bail()
+				.run(req),
+		]);
+
+		// validation Error
+		// todo: Error model 정의하기
+		const validationErrors = validationResult(req);
+		if (!validationErrors.isEmpty()) {
+			validationErrors.array().forEach((value) => {
+				console.log(value);
+			});
+			throw new Error('error');
+		}
+
+		const { user } = res.locals;
+		const { taskId } = req.params;
+		const { contents, tags, period, important } = req.body;
+		const result = await taskService.updateTask(user, taskId, {
+			contents,
+			tags,
+			period,
+			important,
+		});
+		res.status(200).send(result);
 	} catch (err) {
 		next(err);
 	}
