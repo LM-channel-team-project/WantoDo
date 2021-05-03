fa658d87c80a46aa52c351fe37af398fa7ea1fbc
castleMo <eodah4115@gmail.com>
2021-05-03 16:47:07 +0900

    [Add] tag 생성 API
    - validator 설정
    - service 로직 생성
    - docs 설정

--- back/src/controllers/v1/tag.controller.ts
+++ back/src/controllers/v1/tag.controller.ts
@@ -1,9 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
-import { header, validationResult } from 'express-validator';
+import { body, header, validationResult } from 'express-validator';
 import tagService from '../../services/v1/tag.service';
 
 //  21/4/29 by 현빈 - 임시코드 - createTag
 
+/**
+ * @author 강성모(castleMo)
+ * @since 21/05/03
+ *
+ * @param req 	Request
+ * @param res 	Response
+ * @param next	NextFunction
+ */
 export const createTag = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		await Promise.all([
@@ -15,6 +23,22 @@ export const createTag = async (req: Request, res: Response, next: NextFunction)
 				.isJWT()
 				.withMessage('is not JWT value')
 				.run(req),
+			body('name')
+				.trim()
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isString()
+				.withMessage('is not String value')
+				.run(req),
+			body('color')
+				.trim()
+				.notEmpty()
+				.withMessage('is empty')
+				.bail()
+				.isHexColor()
+				.withMessage('is not HexColor value')
+				.run(req),
 		]);
 
 		// validation Error
@@ -23,10 +47,11 @@ export const createTag = async (req: Request, res: Response, next: NextFunction)
 		if (!validationErrors.isEmpty()) {
 			throw new Error('updateUserSettings validationError');
 		}
-		// 유저가 작성한 내용 받기
+
+		const { user } = res.locals;
 		const { name, color } = req.body;
 
-		const result = await tagService.createTag(name, color);
+		const result = await tagService.createTag(user, name, color);
 		res.status(200).send(result);
 	} catch (err) {
 		next(err);
--- back/src/routes/v1/tag.route.ts
+++ back/src/routes/v1/tag.route.ts
@@ -1,17 +1,45 @@
 import { Router } from 'express';
-import { createTag } from '../../controllers/v1/tag.controller';
-
-
+import tagController from '../../controllers/v1/tag.controller';
 
 // 라우터 토대 생성 21/4/29 by 현빈
 const tagRouter = Router();
-// tag 생성
-tagRouter.post('/', createTag);
+
+/**
+ * @author 강성모(castleMo)
+ * @since 21/05/03
+ */
+/**
+ * @api {post} /v1/tags tag 생성
+ * @apiName createTag
+ * @apiGroup Tags
+ * @apiVersion 1.0.0
+ *
+ * @apiUse HeaderToken
+ *
+ * @apiParam (Body) {String} name 태그 이름
+ * @apiParam (Body) {String} color 태그 색
+ * @apiParamExample {json} RequestBodyExample
+ * {
+ *   "name": "tag1",
+ *   "color": "#000000"
+ * }
+ *
+ * @apiSampleRequest /v1/tags
+ * @apiSuccess (SUCCESS) {String} msg 성공메시지
+ * @apiSuccessExample {json} SuccessResponse
+ * {
+ *     "msg": "success"
+ * }
+ */
+tagRouter.post('/', tagController.createTag);
+
 // tag 읽기
-tagRouter.get('/')
+tagRouter.get('/');
+
 // tag 수정
-tagRouter.patch('/')
+tagRouter.patch('/');
+
 // tag 삭제
-tagRouter.delete('/')
+tagRouter.delete('/');
 
 export default tagRouter;
