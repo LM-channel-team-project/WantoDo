0236b0cd49255b02cc65f386a5c9fb58a6cf5237
castleMo <eodah4115@gmail.com>
2021-05-04 13:02:18 +0900

    [Add] profile 조회 API
    - docs 생성
    - validator 설정
    - service 로직 생성

--- back/src/controllers/v1/profile.controller.ts
+++ back/src/controllers/v1/profile.controller.ts
@@ -1,20 +1,40 @@
 import { NextFunction, Request, Response } from 'express';
 import * as profileService from '../../services/v1/profile.service';
 
+/**
+ * @author 강성모(castleMo)
+ * @since 21/05/04
+ *
+ * @param req 	Request
+ * @param res		Response
+ * @param next	NextFunction
+ */
 export const getProfile = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { user } = res.locals;
-		const result = await profileService.getProfile(user); // 여기있는 getProfile은 service.ts에 있는 함수인듯!!
+		const result = await profileService.getProfile(user);
 		res.status(200).send(result);
 	} catch (error) {
 		next(error);
 	}
 };
 
-export const updateProfile = async (req: Request, res: Response) => {
-	res.json({
-		message: 'Hello',
-	});
+/**
+ * @author 강성모(castleMo)
+ * @since 21/05/04
+ *
+ * @param req 	Request
+ * @param res		Response
+ * @param next	NextFunction
+ */
+export const updateProfile = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { user } = res.locals;
+		const result = await profileService.updateProfile(user);
+		res.status(200).send(result);
+	} catch (error) {
+		next(error);
+	}
 };
 
 export default {
