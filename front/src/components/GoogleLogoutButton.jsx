0054e573e6fb4ceda02c4c9ac6512a5cc0a5a169
YoungRong.Oh <youngrong.oh@gmail.com>
2021-05-04 15:29:20 +0900

    [Add] profileModal에 로그아웃 버튼 추가

---
+++ front/src/components/GoogleLogoutButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+import { useHistory } from 'react-router-dom';
+import Button from './Button';
+
+const GoogleLogoutButton = ({ onLogout }) => {
+  const history = useHistory();
+
+  const responseGoogle = () => {
+    console.log('logout');
+    history.push('/login');
+  };
+
+  return (
+    <GoogleLogout
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+      onLogoutSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      // isSignedIn
+      cookiePolicy="single_host_origin"
+      render={() => (
+        <Button styleName="profileModal" onClick={onLogout}>
+          로그아웃
+        </Button>
+      )}
+    />
+  );
+};
+
+export default GoogleLogoutButton;
