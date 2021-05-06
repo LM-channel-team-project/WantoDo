51c6b3b8b3b5c4fbc0fd2bc0f104d3caa6941f07
hae-on <solbi2004@naver.com>
2021-05-06 14:50:55 +0900

    [Add] 캘린더 스타일링

--- front/src/components/Calendar.jsx
+++ front/src/components/Calendar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 import useCalendar from './useCalendar';
 import IconButton from './IconButton';
+import '../styles/Calendar.css';
 
 const Calendar = () => {
   const testStyle = {
@@ -24,60 +25,65 @@ const Calendar = () => {
 
   return (
     <div>
-      <p>
-        Selected Month: {`${monthNames[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}
-      </p>
-      <table className="table">
-        <thead>
-          <tr>
-            {daysShort.map((day) => (
-              <th key={day}>{day}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {Object.values(calendarRows).map((cols) => {
-            return (
-              <tr key={cols[0].date}>
-                {cols.map((col) =>
-                  col.date === todayFormatted ? (
-                    <td>
-                      <IconButton
-                        style={testStyle}
-                        type="button"
-                        pkey={col.date}
-                        className={`${col.classes} today`}
-                        onClick={() => dateClickHandler(col.date)}
-                      >
-                        {col.value}
-                      </IconButton>
-                    </td>
-                  ) : (
-                    <td>
-                      <IconButton
-                        style={testStyle}
-                        type="button"
-                        key={col.date}
-                        className={col.classes}
-                        onClick={() => dateClickHandler(col.date)}
-                      >
-                        {col.value}
-                      </IconButton>
-                    </td>
-                  ),
-                )}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="monthyear">
+        <span className="month">{`${monthNames[selectedDate.getMonth()]}`}</span>
+        <span className="year">{`${selectedDate.getFullYear()}`}</span>
+      </div>
+      <div className="content">
+        <IconButton className="button" styleName="arrowbutton" onClick={getPrevMonth} type="button">
+          <IoIosArrowForward />
+        </IconButton>
+        <table className="table">
+          <thead>
+            <tr className="dayofweek">
+              {daysShort.map((day) => (
+                <th key={day}>{day}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="date">
+            {Object.values(calendarRows).map((cols) => {
+              return (
+                <tr key={cols[0].date}>
+                  {cols.map((col) =>
+                    col.date === todayFormatted ? (
+                      <td>
+                        <IconButton
+                          style={testStyle}
+                          type="button"
+                          pkey={col.date}
+                          className={`${col.classes} today`}
+                          styleName="today"
+                          onClick={() => dateClickHandler(col.date)}
+                        >
+                          {col.value}
+                        </IconButton>
+                      </td>
+                    ) : (
+                      <td>
+                        <IconButton
+                          style={testStyle}
+                          type="button"
+                          key={col.date}
+                          className={col.classes}
+                          styleName="date"
+                          onClick={() => dateClickHandler(col.date)}
+                        >
+                          {col.value}
+                        </IconButton>
+                      </td>
+                    ),
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
 
-      <IconButton className="button" onClick={getPrevMonth} type="button">
-        <IoIosArrowForward />
-      </IconButton>
-      <IconButton className="button" onClick={getNextMonth} type="button">
-        <IoIosArrowBack />
-      </IconButton>
+        <IconButton className="button" styleName="arrowbutton" onClick={getNextMonth} type="button">
+          <IoIosArrowBack />
+        </IconButton>
+      </div>
     </div>
   );
 };
