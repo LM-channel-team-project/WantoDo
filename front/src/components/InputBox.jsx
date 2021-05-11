77429715d2fde998c5dafa7657a49fe9141115ae
YoungRong.Oh <youngrong.oh@gmail.com>
2021-05-11 14:38:44 +0900

    [Style] InputBox 오타 수정
    - styleName 오타 수정
    - 잘못된 주석 수정

--- front/src/components/InputBox.jsx
+++ front/src/components/InputBox.jsx
@@ -5,16 +5,14 @@ import styles from '../styles/InputBox.module.css';
 /**
  * 재사용성을 높인 인풋 박스
  * @param {string} props.labelText - 인풋의 라벨로 표시할 문자열
- * @param {stirng} props.inputType - input의 type 속성 값
- * @param {string} props.inputName - form에서 모든 input의 값을 객체로 받을 때 key로 쓰일 문자열
  * @param {string} props.afterText - input 뒤에 부가 설명을 표시하는 문자열
- * @param {string} props.placeholder - input의 placeholder 속성 값
- * @param {Function} props.validator - input의 유효성 검사를 수행할 콜백 함수, 반환하는 boolean 값에 따라 상태 변화 여부 결정
+ * @param {string} props.styleName - string, 기본 스타일을 변형할 클래스 이름 문자열
  */
 const InputBox = (props) => {
+  const { styleName } = props;
   const { labelText, afterText } = props;
   return (
-    <label className={`${styles.inputBox} ${styles.styleName}`} htmlFor={styles.label}>
+    <label className={`${styles.inputBox} ${styles[styleName]}`} htmlFor={styles.label}>
       <span className={styles.name}>{labelText || ''}</span>
       <Input {...props} />
       <span className={styles.text}>{afterText}</span>
