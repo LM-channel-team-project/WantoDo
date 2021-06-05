4a3e98c3b481f4b518e38ab22740240af1e42dc1
YoungRong.Oh <youngrong.oh@gmail.com>
2021-06-05 20:30:22 +0900

    [Chore] 배포 관련 세팅

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,7 +5,6 @@ import './styles/App.css';
 
 function App() {
   const [isSignIn, setIsSignIn] = useState(false);
-
   return (
     <div className="container">
       {isSignIn ? <Main changeSignState={setIsSignIn} /> : <Login changeSignState={setIsSignIn} />}
